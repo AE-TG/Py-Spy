b8344496bd516a19a98421938d61650daf6128d1
AE-TG <tgillani@andrew.cmu.edu>
2025-02-24 11:24:25 -0500

    Only compile and marshal @spy files

--- src/spy.ts
+++ src/spy.ts
@@ -29,56 +29,98 @@ export function scanUI(ctx: vscode.ExtensionContext) {
 function runAll(ctx: vscode.ExtensionContext) {
     //TODO check timing loop and adjust or run on user trigger
 
+    // Start producing a list of all decorated functions in the workspace.
+    // TODO This is asynchronous so later operations may operate on stale data.
+    updateSpyFns();
+
     // Precompile python code as a simple way to check syntax errors.
     pyCompile();
 
     // Perform static analysis.
     analyze(ctx);
     //TODO yes this is unbelievably drastically simplified for now
-    let decoratedFns: Array<(data: string) => void> = [];
-    decoratedFns.push(pyCompile);
 
     // For decorated functions, test interesting input values to check for edge cases or unintended values/throws.
-    //TODO
+    functionAnalysis(ctx);
+    
+    /*
     decoratedFns.forEach(fn => {
         let inputTypes = spyInputs.getInputTypes(fn);
         if (spyInputs.isPODInput(inputTypes)) {
             spyInputs.testInputs(fn, spyInputs.generateInputs(inputTypes));
         }
     });
+    */
 
     // Compute some statistics.
     getStatistics();
 }
 
+type decoFnType = [file: string, func: string, ...args: string[]]
+let spyFnList: decoFnType[] = [];
+function updateSpyFns() {
+    let promisedList: Promise<decoFnType[]> = findSpyDecorators();
+    promisedList.then(decoFnList => {
+        spyFnList = decoFnList;
+    });
+}
+
+async function findSpyDecorators() {
+    let decoFnList: decoFnType[] = [];
+    let pyFiles = vscode.workspace.findFiles("**/*.py");
+    (await pyFiles).forEach(async file => {
+        let doc: vscode.TextDocument = await vscode.workspace.openTextDocument(file);
+        for(var lineIndex = 1; lineIndex < doc.lineCount; lineIndex++) {
+            if (doc.lineAt(lineIndex).text.startsWith("def ")) {
+                if (doc.lineAt(lineIndex - 1).text.startsWith("@spy")) {
+                    let fName: string = trimFunctionName(doc.lineAt(lineIndex).text);
+                    decoFnList.push([doc.fileName, fName]);
+                }
+            }
+        }
+    });
+    return decoFnList;
+}
+
 function pyCompile() {
-    vscode.window.showInformationMessage('Compiling in background.');
-    //TODO only compile decorated fns
+    vscode.window.showInformationMessage('PySpy: compiling in background.');
     const opt = {} as vscode.TerminalOptions;
     opt.location = vscode.TerminalLocation.Panel;
     opt.hideFromUser = true;
     opt.isTransient = true;
     const term = vscode.window.createTerminal(opt);
-    term.sendText('python -m compileall .', true);
+    const spySet = new Set(spyFnList.map(decoType => decoType[0]));
+    spySet.forEach(file => {
+        console.log("PySpy: attempting to compile " + file);
+        term.sendText('python -m py_compile ' + file, true);
+    });
     //term.dispose();
 
     //TODO AST here?
 }
 
 function analyze(ctx: vscode.ExtensionContext) {
+    return false;
+}
+
+async function functionAnalysis(ctx: vscode.ExtensionContext) {
     let wsPath = vscode.workspace.rootPath;
     let options = {
         scriptPath: ctx.extensionPath,
         args: ["-f", wsPath+"/__pycache__/main.cpython-312.pyc"]
+        // TODO hardcoded build :(
     };
-    PythonShell.run("src/spyMarshal.py", options).then(messages => {
+
+    let fList: string[] = []
+    await PythonShell.run("src/spyMarshal.py", options).then(messages => {
         messages.forEach(msg => {
-            console.log(msg);
+            console.log("spyMarshal found python function: " + msg);
+            fList.push(msg);
         });
     });
-    
-    //TODO
-    return false;
+    fList = fList.filter((fn) => spyFnList.map(decoType => decoType[1]).includes(fn));
+    console.log(fList);
+    return fList;
 }
 
 function getStatistics() {
@@ -89,4 +131,10 @@ function getStatistics() {
     //TODO nested calls
     //TODO timing?
     return 0;
+}
+
+function trimFunctionName(line: string) {
+    let rv: string = line.split("def ")[1].split("(")[0];
+    console.log(rv);
+    return rv;
 }
--- src/spyMarshal.py
+++ src/spyMarshal.py
@@ -1,10 +1,14 @@
 import argparse
 import marshal
+import types
+
+def split_module(m):
+    return (m.co_consts)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', dest='filename', type=str)
 args = parser.parse_args()
-print(args.filename)
+#print(args.filename)
 
 pyc = open(args.filename, 'rb')
 pyc.seek(16)  # skip over python magic numbers
@@ -13,8 +17,16 @@ pyc.seek(16)  # skip over python magic numbers
 #TODO - python 3.7+ requires 16 bytes
 code_obj = marshal.load(pyc)
 
-print(code_obj.co_name)
-print(code_obj.co_qualname)
-print(code_obj.co_argcount)
-print(code_obj.co_varnames)
-print(code_obj.co_consts)
+#print(code_obj.co_name)
+#print(code_obj.co_qualname)
+#print(code_obj.co_argcount)
+#print(code_obj.co_varnames)
+#print(code_obj.co_consts)
+
+rv = split_module(code_obj)
+for x in rv:
+    if x:
+        print(x.co_name)
+#    print("consts" + x.co_consts)
+#print("consts" + str(rv[0].co_consts))
+
