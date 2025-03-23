f7b27117f8da9f2f56acaa13d029c1caec9b7f24
AE-TG <tgillani@andrew.cmu.edu>
2025-03-23 00:26:37 -0400

    Setting up for actual unit/coverage tests

--- src/spy.ts
+++ src/spy.ts
@@ -1,8 +1,9 @@
 import * as vscode from 'vscode';
 import * as spyCompile from './spyCompile';
-import * as spyStatistics from './spyStatistics';
 import * as spyFS from './spyFS';
 import * as spyInputs from './spyInputs';
+import * as spyStatistics from './spyStatistics';
+import * as spyTesting from './spyTesting';
 import * as spyUI from './spyUI';
 
 
@@ -23,27 +24,35 @@ export function run(ctx: vscode.ExtensionContext, enable: boolean, intervalSecon
 
 export function setupUI(ctx: vscode.ExtensionContext) {
     spyUI.createDecorations(ctx);
-    spyUI.updateDecorations(ctx, 1);
+    spyUI.updateDecorations(ctx, 0);
 }
 export function scanUI(ctx: vscode.ExtensionContext) {
-    spyUI.updateDecorations(ctx, 1000);
+    spyUI.updateDecorations(ctx);
 }
-export function scanCC(fileName: string = "") {
-    if (fileName) {
-        spyStatistics.generateRadonCache([fileName]);
+export function scanCC(ctx?: vscode.ExtensionContext, doc?: vscode.TextDocument) {
+    if (ctx && doc) {
+        // A specific file was opened or saved.
+
+        spyUI.updateDecorations(ctx, 0);
+        spyStatistics.generateRadonCache([doc.fileName]);
+        spyCompile.build(doc);
+        // TODO
+        spyCompile.functionAnalysis(ctx); // For decorated functions, test interesting input values to check for edge cases or unintended values/throws.
+        // end TODO
+        spyTesting.generateTestResults(spyUI.getSpyDecos(doc));
+        spyUI.updateDecorations(ctx); // update again to collect the test results
     }
     else {
         spyStatistics.generateRadonCache(spyFS.getSpyFiles());
+        spyCompile.build();
     }
 }
 export function provideHover(file: vscode.TextDocument, pos: vscode.Position, cancel: vscode.CancellationToken) : vscode.ProviderResult<vscode.Hover> {
-    for (let highlight of spyUI.getSpyDecos()) {
-        if (highlight[0] == file) {
-            if (highlight[1].contains(pos)) {
-                return new Promise<vscode.Hover>(resolve => {
-                    resolve(spyStatistics.getComplexity(highlight, cancel));
-                });
-            }
+    for (let highlight of spyUI.getSpyDecos(file)) {
+        if (highlight[1].contains(pos)) {
+            return new Promise<vscode.Hover>(resolve => {
+                resolve(spyStatistics.getComplexity(highlight, cancel));
+            });
         }
     };
     return null;
@@ -51,16 +60,6 @@ export function provideHover(file: vscode.TextDocument, pos: vscode.Position, ca
 
 function runAll(ctx: vscode.ExtensionContext) {
     //TODO check timing loop and adjust or run on user trigger
-
-    // Precompile python code as a simple way to check syntax errors.
-    spyCompile.build();
-
-    // Perform static analysis.
-    analyze(ctx);
-    //TODO yes this is unbelievably drastically simplified for now
-
-    // For decorated functions, test interesting input values to check for edge cases or unintended values/throws.
-    spyCompile.functionAnalysis(ctx);
 
     /*
     decoratedFns.forEach(fn => {
@@ -71,22 +70,5 @@ function runAll(ctx: vscode.ExtensionContext) {
     });
     */
 
-    // Compute some statistics.
-    getStatistics();
 }
-
-function analyze(ctx: vscode.ExtensionContext) {
-    return false;
-}
-
-function getStatistics() {
-    //TODO complexity
-    //TODO code coverage
-    //TODO % decorated functions
-    //TODO const literals
-    //TODO nested calls
-    //TODO timing?
-    return 0;
-}
-
 
--- src/spyTesting.ts
+++ src/spyTesting.ts
@@ -1,13 +1,43 @@
+import * as spyCompile from './spyCompile';
+import * as spyUI from './spyUI';
 import * as vscode from 'vscode';
 
-
-export function generateTestCache(files: Set<string> | string[]) {
+// !! IMPORTANT !!
+// It is on the calling function to provide only a set of inputs for which spyCompile has already built pycache files!
+// Otherwise testing will fail because we don't make bytecode for marshaling to run on here in order to save effort
+export function generateTestResults(decoFunctions: spyUI.spyDeco[]) {
     let cfg = vscode.workspace.getConfiguration("spy");
     let doTests = cfg.get("TestingEnabled", false);
 
     if (doTests) {
-
+        decoFunctions.forEach(fn => {
+            unitTest(fn);
+        });
     }
 }
 
-
+function unitTest(fn: spyUI.spyDeco) {
+    let fName: string = fn[0].lineAt(fn[2]).text;
+    console.log("testing " + fName + " in " + fn[0].fileName);
+    
+    // TODO compile file
+    
+    // TODO generate inputs
+    
+    // TODO run test
+    
+    // TODO catch/report errors
+    /*
+    rvs.forEach(rv => {
+       if (not type) {
+        vscode.window.showInformationMessage('PySpy: compiling in background.');
+       } 
+    });
+    */
+    
+    // TODO update coverage outputs
+    /*
+    spyUI.addCoverageDeco(line, true); // pass
+    spyUI.addCoverageDeco(line, fail); // pass
+    */
+}
