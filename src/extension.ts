e04298fd792b664c460de15eb6f5e44af4216b6d
AE-TG <tgillani@andrew.cmu.edu>
2025-03-25 13:48:41 -0400

    Add cache cleanup command.

--- src/extension.ts
+++ src/extension.ts
@@ -34,23 +34,40 @@ export function activate(context: vscode.ExtensionContext) {
 			spy.scanCC(context, event);
 		}
 	}, null, context.subscriptions);
+
 	
-	vscode.languages.registerHoverProvider('python', {
+	context.subscriptions.push(vscode.commands.registerCommand('spy.PurgeCachedFiles', () => {
+		spy.deleteCache();
+		vscode.window.showInformationMessage('PySpy: Deleting cached files.');
+	}));
+
+	context.subscriptions.push(vscode.languages.registerHoverProvider('python', {
 		provideHover(document, position, token) {
 			return spy.provideHover(document, position, token);
 		}
-	});
+	}));
+
 }
 
 export async function deactivate(): Promise<any> {
+	/*
+	https://github.com/microsoft/vscode/issues/144118
+
+	VSCode made questionable design decisions to tie practically all processes to the renderer,
+	which is killed almost immediately on exit. This means the majority of the API calls
+	have no thread to service them despite the 5-second guarantee on `async deactivate()`.
+	This github issue is the *only* mention of this affecting the API.
+
+	Awaiting cleanup on exit will fail because every file cleanup attempt's promise will be cancelled,
+	causing the extension host process to complain after 1 second of an unhandled cancellation.
+	VSCode, YOU ARE DOING THE CANCELLATION. HANDLE IT YOURSELF.
+	
+	The user can manually trigger a cache cleanup with the command "PySpy: Clean File Cache" in the
+	command palette menu (`Ctrl+Shift+P`).
+
+	We still make an attempt to clean up after ourselves regardless in case the user
+	removes/uninstalls this extension in an instance that remains running.
+	*/
 	spy.removeUI();
-	// https://github.com/microsoft/vscode/issues/144118
-	// VSCode made questionable design decisions to tie practically all processes to the renderer,
-	// which is killed almost immediately on exit. This means the majority of the API calls
-	// have no thread to service them despite the 5-second guarantee on `async deactivate()`.
-	// Awaiting cleanup on exit will fail because every file cleanup attempt's promise will be cancelled,
-	// causing the extension host process to complain after 1 second of an unhandled cancellation.
-	// This github issue is the *only* mention of this affecting the API.
-	// VSCode, YOU ARE DOING THE CANCELLATION. HANDLE IT YOURSELF.
 	return spy.deleteCache();
 }
